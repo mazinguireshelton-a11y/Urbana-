fff97ab24bdcac1a520a9ae257bf3b94d8ea3ba8
Shelton mazinguire <mazinguireshelton@gmail.com>
2026-04-18 06:08:02 +0200

    Change redirect target in index route

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def create_comparison_chart(areas, water_values, energy_values):
 @app.route("/")
 def index():
     """Página inicial - redireciona para dashboard sem login"""
-    return redirect(url_for('index'))
+    return redirect(url_for('base'))
 
 @app.route("/login")
 def login():
